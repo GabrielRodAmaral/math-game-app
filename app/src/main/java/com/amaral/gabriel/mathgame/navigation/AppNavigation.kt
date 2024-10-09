c0fab33ee6fe451d1b32a14f1d31c693a2a3bf09
GabrielRodAmaral <gabriel.amaral@sptech.school>
2024-10-09 20:12:17 -0300

    Refactored Game Screen to follow the architecture patterns

--- app/src/main/java/com/amaral/gabriel/mathgame/navigation/AppNavigation.kt
+++ app/src/main/java/com/amaral/gabriel/mathgame/navigation/AppNavigation.kt
@@ -2,12 +2,14 @@ package com.amaral.gabriel.mathgame.navigation
 
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.amaral.gabriel.mathgame.screen.HomeScreen
-import com.amaral.gabriel.mathgame.screen.GameScreen
 import com.amaral.gabriel.mathgame.screen.ResultScreen
+import com.amaral.gabriel.mathgame.ui.screen.GameScreen
+import com.amaral.gabriel.mathgame.ui.viewmodel.GameViewModel
 
 @Composable
 fun AppNavigation(modifier: Modifier = Modifier) {
@@ -20,7 +22,8 @@ fun AppNavigation(modifier: Modifier = Modifier) {
 
         composable(Router.GameScreen.route) { backStackEntry ->
             val category = backStackEntry.arguments?.getString("category")?.toString() ?: ""
-            GameScreen(modifier, navController, category)
+            val viewModel: GameViewModel = viewModel(backStackEntry)
+            GameScreen(modifier = modifier, navController = navController, category = category, viewModel = viewModel)
         }
 
         composable(Router.ResultScreen.route) { backStackEntry ->
